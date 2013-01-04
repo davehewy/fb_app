3053fa8200a5d8910ac71ec4befd93d072a11fae
David Heward <dave@bytewire.co.uk>
2013-01-04 10:14:26 +0000

    adding link comment

---
+++ submit.php
@@ -0,0 +1,19 @@
+<?php include_once("inc/header.php"); ?>
+
+<script type="text/javascript">$(function(){$(".bg").css({"background":"none"});});</script>
+
+<div class="orange_content">
+	<h1>Confirmation and thanks heading text</h1>
+	<p>A few words confirming end of gameplay at psum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut adipiscing elit, sed diam nonummy nibh euismod tincidunt.</p>
+	<h2>Terms and Conditions</h2>
+	<p>Second paragraph explaing terms at psum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut adipiscing elit, sed diam nonummy nibh euismod tincidunt.</p>
+	<ul>
+		<li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</li>
+		<li>Nipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</li>
+		<li>Consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</li>
+		<li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</li>
+	</ul>
+</div>
+
+<?php include_once("inc/footer.php"); ?>
+
